67cf2002692bc85ab603bc92bae2cf82168f0121
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 22:23:06 +0300

    changes made to App.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Deposit from './Components/DepositForm'
-import GoalsForm from './Components/GoalForm'
-import GoalsList from './Components/GoalList'
-import Overview from './Components/Overview'
 import App from './App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Deposit />
-    <GoalsForm />
-    <GoalsList/>
-    <Overview />
     <App/>
   </StrictMode>,
 )
