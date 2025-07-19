fbc1e56efe8c7a3fb36db92f8d35991898492d5b
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 19:49:24 +0300

    changes made to GoalsForm.jsx file

--- src/Components/GoalForm.jsx
+++ src/Components/GoalForm.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+
 function GoalsForm(){
+
+    const [formData, setFormData] = useState({
+        Name:"",
+        Targetamount:"",
+        Category:"",
+        Deadline:""
+    })
+
+function changeAction(e){
+    setFormData({formData,[e.target.name]: e.target.value})
+}
+
+function submitAction(e){
+    e.preventDefault();
+    const newGoal = {
+        formData,
+        Targetamount: parseFloat(formData.Targetamount),
+        savedAmount: 0,
+    }
+    onAddGoal(newGoal)
+    setFormData({name:"", Targetamount:"", Category:"", Deadline:""})
+}
+
     return(
         <div>
-
+            <div><button>Delete</button></div>
         </div>
     )
 }
