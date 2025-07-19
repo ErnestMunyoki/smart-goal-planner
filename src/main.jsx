7d4a4abbcea69aac897ec45321e47b6139ac52ce
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 22:27:37 +0300

    changes made to Main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Deposit from './Components/DepositForm'
 import App from './App'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Deposit />
     <App/>
   </StrictMode>,
 )
