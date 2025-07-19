ab2382eb8abe2a88e7512b2554f75dd479bd6523
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 21:56:49 +0300

    changes made to Overview.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
 import GoalForm from "./Components/GoalForm";
 import GoalList from "./Components/GoalList";
+import DepositForm from "./Components/DepositForm";
+import Overview from "./Components/Overview";
 
 function App() {
-  const [goals, setGoals] = useState([]); 
+  const [goals, setGoals] = useState([]);
 
-  const handleAddGoal = (goal) => {
-    const newGoalWithId = { ...goal, id: Date.now() };     
-    setGoals([...goals, newGoalWithId]);
-    console.log("New Goal Submitted:", newGoalWithId);
-  };
+  function addGoal(goal) {
+    const newGoal = { ...goal, id: Date.now() };
+    setGoals([...goals, newGoal]);
+  }
 
-  const handleDeleteGoal = (id) => {
-    const updatedGoals = goals.filter(goal => goal.id !== id);
-    setGoals(updatedGoals);
-  };
+  function deleteGoal(id) {
+    setGoals(goals.filter(goal => goal.id !== id));
+  }
 
+  function addDeposit(goalId, amount) {
+    setGoals(goals.map(goal =>
+      goal.id === goalId
+        ? { ...goal, savedAmount: goal.savedAmount + amount }
+        : goal
+    ));
+  }
 
+  return (
+    <div>
+      <h1>Smart Goal Planner</h1>
+      <GoalForm onAddGoal={addGoal} />
+      <Overview goals={goals} />
+      {goals.map(goal => (
+        <DepositForm key={goal.id} goalId={goal.id} onAddDeposit={addDeposit} />
+      ))}
+      <GoalList goals={goals} onDeleteGoal={deleteGoal} />
+    </div>
+  );
 }
 
 export default App;
-
-
