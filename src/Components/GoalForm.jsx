13f5c6ab1a0ef009c160cdd56d7c15cf2a7d61a9
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 20:37:34 +0300

    changes made to GoalForm.jsx

--- src/Components/GoalForm.jsx
+++ src/Components/GoalForm.jsx
@@ -1,43 +1,45 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function GoalForm({onAddGoal}){
+function GoalForm({ onAddGoal }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    targetAmount: "",
+    category: "",
+    deadline: "",
+  });
 
-    const [formData, setFormData] = useState({
-        name:"",
-        targetAmount:"",
-        category:"",
-        deadline:""
-    })
+  function changeAction(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
-function changeAction(e){
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-}
-
-function submitAction(e){
+  function submitAction(e) {
     e.preventDefault();
     const newGoal = {
-        name: formData.name,
-        targetAmount: parseFloat(formData.targetAmount),
-        category: formData.category,
-        deadline: formData.deadline,
-        savedAmount: 0
+      name: formData.name,
+      targetAmount: parseFloat(formData.targetAmount),
+      category: formData.category,
+      deadline: formData.deadline,
+      savedAmount: 0,
     };
-    onAddGoal(newGoal)
-    setFormData({name:"", targetAmount:"", category:"", deadline:""})
+    onAddGoal(newGoal);
+    setFormData({
+      name: "",
+      targetAmount: "",
+      category: "",
+      deadline: "",
+    });
+  }
+
+  return (
+    <form onSubmit={submitAction}>
+      <h1>Add New Goal</h1>
+      <input name="name" placeholder="Goal name" value={formData.name} onChange={changeAction} />
+      <input name="targetAmount" placeholder="Target Amount" value={formData.targetAmount} onChange={changeAction} />
+      <input name="category" placeholder="Category" value={formData.category} onChange={changeAction} />
+      <input name="deadline" placeholder="Deadline (DD-MM-YYYY)" value={formData.deadline} onChange={changeAction} />
+      <button type="submit">Add Goal</button>
+    </form>
+  );
 }
 
-    return(
-        <div>
-            <form onSubmit={submitAction}>
-                <h1>Add New Goal</h1>
-                <input name="name" placeholder="Goal name" value={formData.name || ""} onChange={changeAction}/>
-                <input name="targetAmount" placeholder="Target Amount" value={formData.targetAmount ||""} onChange={changeAction} />
-                <input name="category" placeholder="Category" value={formData.category ||""} onChange={changeAction} />
-                <input name="deadline" placeholder="Deadline (DD-MM-YYYY)" value={formData.deadline ||""} onChange={changeAction}/>
-                <button type="Submit">Add Goal</button>
-            </form>
-        </div>
-    )
-}
-
-export default GoalForm
+export default GoalForm;
