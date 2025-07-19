32b41d68fb674871309677154871771025ff7b62
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 22:10:15 +0300

    changes made to App.jsx and Overview,jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
 import { useState } from "react";
 import GoalForm from "./Components/GoalForm";
 import GoalList from "./Components/GoalList";
-import DepositForm from "./Components/DepositForm";
 import Overview from "./Components/Overview";
 
 function App() {
   const [goals, setGoals] = useState([]);
 
-  function addGoal(goal) {
-    const newGoal = { ...goal, id: Date.now() };
-    setGoals([...goals, newGoal]);
-  }
+  const handleAddGoal = (goal) => {
+    const newGoalWithId = { ...goal, id: Date.now() };
+    setGoals([...goals, newGoalWithId]);
+  };
 
-  function deleteGoal(id) {
-    setGoals(goals.filter(goal => goal.id !== id));
-  }
+  const handleDeleteGoal = (id) => {
+    const updatedGoals = goals.filter((goal) => goal.id !== id);
+    setGoals(updatedGoals);
+  };
 
-  function addDeposit(goalId, amount) {
-    setGoals(goals.map(goal =>
-      goal.id === goalId
-        ? { ...goal, savedAmount: goal.savedAmount + amount }
-        : goal
-    ));
-  }
-
- 
 }
 
 export default App;
+
