21c6b7209655a273e1184a47aa727777dc5f5007
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-19 20:51:44 +0300

    changes made to GoalList.jsx file

--- src/Components/GoalList.jsx
+++ src/Components/GoalList.jsx
@@ -1,9 +1,24 @@
-function GoalsList(){
+function GoalList({goals, onDeleteGoal}) {
     return(
         <div>
-
+             <h2>Your Goals</h2>
+             {goals.length === 0 ? (
+                <p>No goals yet</p>
+             ) : (
+                goals.map((goal) => (
+                    <div>
+                        <h3>{goal.name}</h3>
+                        <p>Target: ${goal.targetAmount}</p>
+                        <p>Saved: ${goal.savedAmount}</p>
+                        <p>Remaining: ${goal.targetAmount - goal.savedAmount}</p>
+                        <p>Deadline: ${goal.deadline}</p>
+                        <button onClick={() => onDeleteGoal(goal.id)}>Delete</button>
+                    </div>
+                ))
+             )
+            }
         </div>
     )
 }
 
-export default GoalsList
+export default GoalList
